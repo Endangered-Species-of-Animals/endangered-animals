33612bd3adab6b6742364b5df7b3b3e05bacd675
Sebas Molina <seba.molina.2013@gmail.com>
2022-06-01 20:47:48 -0300

    entity & enum creation

---
+++ demo/src/main/java/com/esoa/demo/entity/User.java
@@ -0,0 +1,13 @@
+package com.esoa.demo.entity;
+
+import com.esoa.demo.enumeration.Role;
+
+public class User {
+    private int id;
+    private String nickname;
+    private String name;
+    private String lastName;
+    private String password;
+    private Role role;
+    private boolean deleted;
+}
