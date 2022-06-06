339876ebe2da38575881b18a40aab17b46fe7d9e
Sebas Molina <seba.molina.2013@gmail.com>
2022-06-06 18:43:51 -0300

    annotations, repositories, services

--- demo/src/main/java/com/esoa/demo/entity/Animal.java
+++ demo/src/main/java/com/esoa/demo/entity/Animal.java
@@ -2,18 +2,11 @@ package com.esoa.demo.entity;
 
 import com.esoa.demo.enumeration.Category;
 import java.time.LocalDate;
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.persistence.*;
+
 import static javax.persistence.EnumType.STRING;
-import javax.persistence.Enumerated;
 import static javax.persistence.FetchType.EAGER;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,8 +14,8 @@ import org.hibernate.annotations.SQLDelete;
 
 
 @Entity
-@Table(name = "animal")
-@SQLDelete(sql = "UPDATE animal SET deleted = true WHERE id = ?")
+@Table(name = "animal", indexes = {@Index(name = "idx_email", columnList = "email")})
+//@SQLDelete(sql = "UPDATE animal SET deleted = true WHERE id = ?")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -30,12 +23,13 @@ public class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name = "name", length = 20)
+    @Column(name = "name")
     private String name;
     private String scientific_name;
     @Lob
+    @Column(name = "post_description", columnDefinition="CLOB", nullable = false)
     private String description;
-    @Column(name = "dischar_date")
+    @Column(name = "discharge_date")
     private LocalDate dischargeDate;
     @Enumerated(STRING)
     @Column(name = "category", nullable = false)
--- demo/src/main/java/com/esoa/demo/entity/Specie.java
+++ demo/src/main/java/com/esoa/demo/entity/Specie.java
@@ -1,13 +1,31 @@
 
 package com.esoa.demo.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "specie")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Specie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "specie_name", nullable = false)
     private String name;
+    @Column(name = "specie_kingdom",nullable = false)
     private String kingdom;
+    @Column(name = "specie_phylum",nullable = false)
     private String phylum;
+    @Column(name = "specie_order", nullable = false)
     private String order;
+    @Column(name = "specie_family", nullable = false)
     private String family;
+    @Column(name = "specie_genus", nullable = false)
     private String genus;
 }
