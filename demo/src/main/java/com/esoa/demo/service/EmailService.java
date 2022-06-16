6679cec2195857d17b5999c3d0b0b6bb72f7f64d
Sebas Molina <seba.molina.2013@gmail.com>
2022-06-16 12:21:23 -0300

    mail dependecies

--- demo/src/main/java/com/esoa/demo/service/EmailService.java
+++ demo/src/main/java/com/esoa/demo/service/EmailService.java
@@ -1,21 +1,25 @@
 package com.esoa.demo.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-//import org.springframework.mail.javamail.JavaMailSender;
+
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
-    //private final JavaMailSender sender;
-    //AÑADIR DEPENDENCIA MAIL EN POM.XML
-    /*
-    @Value("${spring.mail.username}")
+    private final JavaMailSender sender;
+
+    @Value("endangered.species.of.animals@gmail.com")
     private String from;
 
     private static final String SUBJECT = "Welcome email";
     private static final String TEXT = "Welcome to our page. Thank you for registering!";
-
+    private static final String COMMENT = "Thank you for contacting us. We will read your comments soon.";
+    
     @Async
     public void send(String to) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -25,5 +29,15 @@ public class EmailService {
         message.setText(TEXT);
         sender.send(message);
     }
-    */
+
+    @Async
+    public void sendGreateful(String to) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setFrom(from);
+        message.setSubject(SUBJECT);
+        message.setText(COMMENT);
+        sender.send(message);
+    }
+
 }
